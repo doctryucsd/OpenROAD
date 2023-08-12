da9b3e7d864595d2ac43f087fb8ca9162c9e85b2
doctryucsd <chy036@ucsd.edu>
2023-08-11 17:02:35 -0700

    more flexibility

--- src/stt/test/convert.sh
+++ src/stt/test/convert.sh
@@ -1,12 +1,17 @@
-points=(50 100 200 500 800 1000 2000 5000)
+points=(50    100   200   500   800   1000  2000  5000)
+canvas=(10000 10000 10000 10000 10000 10000 10000 40000)
+# points=(8000 10000 20000)
+# points=(100000)
 dist="mix-normal"
 path="/home/chy036/NN-Steiner/points"
 
 for i in "${!points[@]}"; do
     num=${points[$i]}
+    range=${canvas[$i]}
+    set=${num}_${range}x${range}
     cmd="python convert.py \
-    $path/point${num}_10000x10000-$dist-100-pt/ \
-    ./test_sets/${num}_10000x10000-$dist.nets"
+    $path/point$set-$dist-100-pt/ \
+    ./test_sets/$set-$dist.nets"
     echo $cmd
     $cmd
 done
--- src/stt/test/exp.sh
+++ src/stt/test/exp.sh
@@ -1,12 +1,16 @@
-points=(50 100 200 500 800 1000 2000 5000)
-# points=(50)
+points=(50    100   200   500   800   1000  2000  5000)
+canvas=(10000 10000 10000 10000 10000 10000 10000 40000)
+# points=(8000 10000 20000)
+# points=(100000)
 dist="mix-normal"
 
 for i in "${!points[@]}"; do
     num=${points[$i]}
-    export file="test_sets/${num}_10000x10000-$dist.nets"
-    export output="outputs/flute${num}_10000x10000-$dist.txt"
-    export tree_dir="trees/flute${num}_10000x10000-$dist/"
+    range=${canvas[$i]}
+    set=${num}_${range}x${range}
+    export file="test_sets/$set-$dist.nets"
+    export output="outputs/flute$set-$dist.txt"
+    export tree_dir="trees/flute$set-$dist/"
     cmd="../../../build/src/openroad get_flute_nets_wirelength.tcl"
     echo $cmd
     $cmd
